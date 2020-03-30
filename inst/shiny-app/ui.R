c489d8297e6f28b7bc0bb4173220f5a55f025bc7
Gopal <gopalpenny@gmail.com>
2020-03-30 10:31:55 -0400

    Added note about timeseries filtering by map

--- inst/shiny-app/ui.R
+++ inst/shiny-app/ui.R
@@ -32,7 +32,7 @@ shinyUI(fluidPage(
 
     hr(),
     fluidRow(
-        column(4,h4("Timeseries and plots"),offset = 4)
+        column(4,h4("Timeseries and plots (filtered by map bounds)"),offset = 4)
     ),
 
 
